8398d97a5d4f5d30f87ac5f5d027a1a781219352
codesmith-247 <adedavid.tech@gmail.com>
2023-01-02 01:10:05 +0000

    Create Opion EHostel Application

---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './components/App';
+
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App />);
+
